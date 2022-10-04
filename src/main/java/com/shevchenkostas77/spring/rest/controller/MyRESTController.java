16280bf0109f00cfc91de54680035444d88dac8b
shevchenkostas77 <shevchenkostas77@icloud.com>
2022-10-04 08:24:31 +0300

    REST: get all employees from database

---
+++ src/main/java/com/shevchenkostas77/spring/rest/controller/MyRESTController.java
@@ -0,0 +1,26 @@
+package com.shevchenkostas77.spring.rest.controller;
+
+import com.shevchenkostas77.spring.rest.entity.Employee;
+import com.shevchenkostas77.spring.rest.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class MyRESTController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping("/employees")
+    public List<Employee> showAllEmployees() {
+        List<Employee> allEmployees = employeeService.getAllEmployees();
+
+        return allEmployees;
+    }
+}
